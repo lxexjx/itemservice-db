ef3f01b8266ecd71e5e02baa01be4bfbaa48dbe2
lxexjx <eun.leee1377@gmail.com>
2022-09-07 12:18:53 +0900

    commit 테스트의 데이터베이스 분리

--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest //SpringBooteApplication을 찾아 ->V3Config로
 class ItemRepositoryTest {
 
     @Autowired
