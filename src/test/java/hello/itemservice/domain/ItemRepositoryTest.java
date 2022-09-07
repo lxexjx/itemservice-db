1bd27b5c27a223572e88790900b523d170157819
lxexjx <eun.leee1377@gmail.com>
2022-09-07 12:38:52 +0900

    commit 데이터 롤백&트랜잭션&임베디드 모드 DB

--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -11,28 +11,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest //SpringBooteApplication을 찾아 ->V3Config로
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
 
-    @Autowired
-    PlatformTransactionManager transactionManager;
-    TransactionStatus status;
-
-    @BeforeEach
-    void beforeEach() {
-        //트랜잭션 시작
-        status = transactionManager.getTransaction(new
-                DefaultTransactionDefinition());
-    }
+//    @Autowired
+//    PlatformTransactionManager transactionManager;
+//    TransactionStatus status;
+//
+//    @BeforeEach
+//    void beforeEach() {
+//        //트랜잭션 시작
+//        status = transactionManager.getTransaction(new
+//                DefaultTransactionDefinition());
+//    }
 
     @AfterEach
     void afterEach() {
@@ -40,8 +42,8 @@ class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
-        //트랜잭션 롤백
-        transactionManager.rollback(status);
+//        //트랜잭션 롤백
+//        transactionManager.rollback(status);
     }
 
     @Test
